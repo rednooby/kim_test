3140deabcf1a181c1a2d2f5b57da838c0a8277b8
smyang1013 <smyang1013@gmail.com>
2016-07-13 01:45:31 +0900

    Neural Network Learn code 90% completed

--- Machine Learning/test code/NeuralNetworkLearn.py
+++ Machine Learning/test code/NeuralNetworkLearn.py
@@ -23,12 +23,13 @@ class Network:
 #def NeuralNetworkLearn(Layers,learnRate,errFunChgLmt,weightChgLmt,maxRound,trainInput,trainDesiredOut,testInput,testDesiredOutput):
 
 #def NeuralNetworkLearn(Layers,errFunChgLmt,weightChgLmt,maxRound,trainInput,trainDesiredOut):
-def NeuralNetworkLearn(Layers,trainInput,trainDesiredOut):
+def NeuralNetworkLearn(Layers,learnRate,errFunChgLmt,weightChgLmt,maxRound,trainInput,trainDesiredOut):
 	inputRow, inputCol = len(trainInput), len(trainInput[0])
 	outputRow, outputCol = len(trainDesiredOut), len(trainDesiredOut[0])
 
 	# initialization
 	network=Network()
+	bestNetwork=Network()
 
 	layerLen = len(Layers)
 	network.layerLen = layerLen
@@ -85,12 +86,39 @@ def NeuralNetworkLearn(Layers,trainInput,trainDesiredOut):
 			out[i+1] = np.array(out[i+1])
 
 		output = np.array(out[layerLen-1][1:])
-
+			# E(w)
 		err = np.mean((output-np.array(trainDesiredOut).transpose()) * (output-np.array(trainDesiredOut).transpose()), axis=0)
+		last_errFun = np.mean(err)
 
-'''
-garbage
-		# forwardProp
+
+		# back Propagation
+		delta = []
+		for i in range(layerLen):
+			delta.append(None)
+		delta[layerLen-1] = deriv_sigmoid(output) * deriv_squared_error(output, np.array(trainDesiredOut).transpose())
+		
+			# calculate delta_input
+		for i in range(layerLen-1,1,-1):
+			x = out[i-1][1:]
+			d = delta[i]
+			delta[i-1] = deriv_sigmoid(x) * np.dot(network.w[i-1].transpose()[1:], d)
+		OutDelta = []
+		for i in range(layerLen):
+			OutDelta.append(None)
+		for i in range(layerLen-1,0,-1):
+			x = out[i-1]
+			d = delta[i]
+			OutDelta[i] = np.dot(d, x.transpose())
+
+		# update
+		weightChg=0
+		for i in range(layerLen-1,0,-1):
+			change = learnRate/m * OutDelta[i]
+			network.w[i-1] = network.w[i-1] - change
+			weightChg = max(weightChg, abs(change).max())
+		round_count = round_count+1
+
+		# forwardProp, check error(train)
 		out = []
 		for i in range(layerLen):
 			out.append(None)
@@ -102,30 +130,70 @@ garbage
 			out[0].append(train)
 		out[0] = np.array(out[0])
 		z=[]
-		for i in layerLen:
-			z.append(None)
-		for i in layerLen-1:
-			z[i+1] = np.dot(network.w[i],out[i]) # z = w*o
-			tmp = []
-			for j in range(m):
-				tmp.append(-1)
-			out[i+1] = [np.array(tmp), sigmoid(z[i+1])] # out=1/(1+e^(-z))
-		output = out[layerLen-1][2:]
-
-		err = np.mean(output-np.array(trainDesiredOut).transpose(), axis=0)
-		last_errFun = np.mean(err)
-
-
-		# backPropagation
-
-		delta = []
 		for i in range(layerLen):
-			delta.append(None)
-		delta[layerLen-1] = deriv_sigmoid(output) * deriv_squared_error(output, np.array(trainDesiredOut).transpose())
-			# calculate delta_input
+			z.append(None)
+		for i in range(layerLen-1):
+			z[i+1] = np.dot(network.w[i],out[i]) # z = w*o
+			temp_list = []
+			for j in range(m):
+				temp_list.append(-1)
+			out[i+1]=[temp_list]
+			for z_val in sigmoid(z[i+1]).tolist():
+				out[i+1].append(z_val)
+			out[i+1] = np.array(out[i+1])
+		output = np.array(out[layerLen-1][1:])
 		
-''' 
+		trainErr = np.mean((output-np.array(trainDesiredOut).transpose()) * (output-np.array(trainDesiredOut).transpose()), axis=0)
+		trainErrFun=np.mean(trainErr)
+		max_values = [i.max() for i in output.transpose()]
+		temp_output = []
+		for i in range(len(output)):
+			temp_output.append(max_values)
+		temp_output = np.array(temp_output)
+		output_match = output == temp_output
+		correct=trainDesiredOut.transpose() == output_match
+		correct_int=[]
+		for i in range(len(correct)):
+			correct_int.append([1 if val==True else 0 for val in correct[i]])
+		correct_int = np.array(correct_int)
+		trainErrFunRate = 1-np.mean(correct_int)
+		
+		#trainErrFun_list=[]
+		#trainErrFun_list.append(trainErrFun)
+		#trainErrFun = trainErrFun_list
+		#trainErrFunRate_list=[]
+		#trainErrFunRate_list.append(trainErrFunRate)
+		#trainErrFunRate = trainErrFunRate_list
+		
+		errFunChg=abs(trainErrFun - last_errFun)
+				
+		if trainErrFun < min_trainErrFun:
+			min_trainErrFun = trainErrFun
+			min_trainErrFunRate = trainErrFunRate
+		
+			bestNetwork = Network()
+			bestNetwork.bestTrainErrFun = min_trainErrFun
+			bestNetwork.bestTrainErrFunRate = min_trainErrFunRate
 
+#	'while errFunChg > errFunChgLmt and weightChg > weightChgLmt and round_count < maxRound:
+
+
+	print 'errFunChg'
+	print errFunChg
+	print 'errFunChgLmt'
+	print errFunChgLmt
+	print 'weightChg'
+	print weightChg
+	print 'weightChgLmt'
+	print weightChgLmt
+	print 'round_count'
+	print round_count
+	print 'maxRound'
+	print maxRound
+
+	print 'while errFunChg > errFunChgLmt and weightChg > weightChgLmt and round_count < maxRound:'
+	print round_count
+	return bestNetwork
 
 
 '''
@@ -137,8 +205,34 @@ trainDesiredOut = np.array([[3],[4],[5]])
 
 
 Layers=[8,4,1]
+errFunChgLmt = 1e-6;
+weightChgLmt = 1e-4;
+maxRound = 1000;
+#maxRound = 2;
+learnRate = 1;
 ###########NeuralNetworkLearn(Layers,trainInput,trainDesiredOut)
+testnetwork = NeuralNetworkLearn(Layers,learnRate,errFunChgLmt,weightChgLmt,maxRound,trainInput,trainDesiredOut)
+'''
+	layerLen=None
+	trainErrFun=None
+	trainErrFunRate=None
+	testErrFun=None
+	testErrFunRate=None
+	w=None
 
+	bestTrainErrFun=None
+	bestTrainErrFunRate=None
+	bestTestErrFun=None
+	bestTestErrFunRate=None
+'''
+print '---------------------------------'
+print testnetwork.w
+print testnetwork.bestTrainErrFun
+print testnetwork.bestTrainErrFunRate
+
+
+
+'''
 
 print '-------------------trainInput-----------'
 print trainInput
@@ -176,6 +270,8 @@ for row in range(temp_w_array.size/temp_w_array[0].size):
 		temp_w_array[row,col] = 1.5
 		####temp_w_array[row,col] = 1-2.0*random.random()
 network.w[layerLen-2] = temp_w_array
+
+print '--network.w--'
 print network.w
 
 for i in range(len(network.w)):
@@ -225,12 +321,14 @@ for i in range(layerLen-1):
 
 output = np.array(out[layerLen-1][1:])
 
+# E(w)
 err = np.mean((output-np.array(trainDesiredOut).transpose()) * (output-np.array(trainDesiredOut).transpose()), axis=0)
-
+last_errFun = np.mean(err)
 
 
 print 'OUTPUT'
 print output
+
 # back Propagation
 delta = []
 for i in range(layerLen):
@@ -240,7 +338,92 @@ delta[layerLen-1] = deriv_sigmoid(output) * deriv_squared_error(output, np.array
 print 'delta[2]'
 print delta[layerLen-1]
 	# calculate delta_input
+for i in range(layerLen-1,1,-1):
+	x = out[i-1][1:]
+	d = delta[i]
+	delta[i-1] = deriv_sigmoid(x) * np.dot(network.w[i-1].transpose()[1:], d)
+OutDelta = []
+for i in range(layerLen):
+	OutDelta.append(None)
+for i in range(layerLen-1,0,-1):
+	x = out[i-1]
+	d = delta[i]
+	OutDelta[i] = np.dot(d, x.transpose())
+
+# update
+weightChg=0
+for i in range(layerLen-1,0,-1):
+	change = learnRate/m * OutDelta[i]
+	network.w[i-1] = network.w[i-1] - change
+	weightChg = max(weightChg, abs(change).max())
+round_count = round_count+1
+
+# forwardProp, check error(train)
+out = []
+for i in range(layerLen):
+	out.append(None)
+out[0] = []
+for i in range(m):
+	out[0].append(-1)
+out[0] = [out[0]]
+for train in trainInput.transpose().tolist():
+	out[0].append(train)
+out[0] = np.array(out[0])
+z=[]
+for i in range(layerLen):
+	z.append(None)
+for i in range(layerLen-1):
+	z[i+1] = np.dot(network.w[i],out[i]) # z = w*o
+
+	temp_list = []
+	for j in range(m):
+		temp_list.append(-1)
+	out[i+1]=[temp_list]
+	for z_val in sigmoid(z[i+1]).tolist():
+		out[i+1].append(z_val)
+	out[i+1] = np.array(out[i+1])
+output = np.array(out[layerLen-1][1:])
+
+trainErr = np.mean((output-np.array(trainDesiredOut).transpose()) * (output-np.array(trainDesiredOut).transpose()), axis=0)
+print '--trainErr--'
+print trainErr
+trainErrFun=np.mean(trainErr)
+print '-------------trainErrFun---------'
+print trainErrFun
+max_values = [i.max() for i in output.transpose()]
+print max_values
+temp_output = []
+for i in range(len(output)):
+	temp_output.append(max_values)
+temp_output = np.array(temp_output)
+output_match = output == temp_output
+correct=trainDesiredOut.transpose() == output_match
+correct_int=[]
+for i in range(len(correct)):
+	correct_int.append([1 if val==True else 0 for val in correct[i]])
+correct_int = np.array(correct_int)
+trainErrFunRate = 1-np.mean(correct_int)
+
+#trainErrFun_list=[]
+#trainErrFun_list.append(trainErrFun)
+#trainErrFun = trainErrFun_list
+#trainErrFunRate_list=[]
+#trainErrFunRate_list.append(trainErrFunRate)
+#trainErrFunRate = trainErrFunRate_list
+
+errFunChg=abs(trainErrFun - last_errFun)
+print trainErrFunRate
+print 'DDone'
 		
+if trainErrFun < min_trainErrFun:
+	min_trainErrFun = trainErrFun
+	min_trainErrFunRate = trainErrFunRate
+
+	bestNetwork = Network()
+	bestNetwork.bestTrainErrFun = min_trainErrFun
+	bestNetwork.bestTrainErrFunRate = min_trainErrFunRate
+
+'''
 
 
 
