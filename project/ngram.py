47d40590045b9f30ea697c54ad462f62aebdf4ec
smyang1013 <smyang1013@gmail.com>
2016-07-15 13:26:10 +0900

    ngram modified

--- project/ngram.py
+++ project/ngram.py
@@ -4,13 +4,16 @@ import time
 
 start_time = time.time()
 
+# arguments option
 if len(sys.argv) is not 3:
 	print >> sys.stderr, 'Usage: python %s [INPUT_FILE] [TARGET]' % sys.argv[0]
 	exit(1)
 
+# input file
 f = open(sys.argv[1], 'r')
 hex_value = f.read().encode('hex')
 
+# make a dictionary of ngram
 ngram_dict = dict()
 
 for i in range(len(hex_value)-8):
@@ -24,39 +27,21 @@ dictList = []
 
 for key, value in ngram_dict.iteritems():
 	temp = []
-	temp.append(key[0:2])
-	temp.append(key[2:4])
-	temp.append(key[4:6])
-	temp.append(key[6:8])
 	temp.append(value)
+	temp.append(key)
 	dictList.append(temp)
 	
-
-
-
-
-
-##############3
-
-
-newlines=dictList
-
-# make dictionary list
-dictList=[]
-for line in newlines:
-	dictList.append({'data':line[0:4], 'freq':int(line[4])})
 # sort
-d2=[(x['freq'],x) for x in dictList]
-d3 = sorted(d2,reverse=True)
-d4 = [y['data'] for (x,y) in d3]
+d1 = sorted(dictList, reverse=True)	
+d2 = d1[0:500]
+d3 = [l[1] for l in d2]
 
+# target file
 fo = open(sys.argv[2], "w")
-for ngram in d4[0:500]:
-	for byte in ngram:
-		fo.write(str(int(byte,16))+' ')
-		#fo.write('%d ' % int(byte,16))
-		#fo.write(byte+' ')
-
+data = ""
+for ngram in d3:
+	data = data+str(int(ngram[0:2],16))+' '+str(int(ngram[2:4],16))+' '+str(int(ngram[4:6],16))+' '+str(int(ngram[6:8],16))+' '
+fo.write(data)
 
 end_time = time.time()
 
